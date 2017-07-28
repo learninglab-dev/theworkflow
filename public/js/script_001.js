59f90a6ccccb11e78586cf2be571d127afef0ca3
Marlon Kuzmick <mkuzmick@mac.com>
2017-07-28 07:35:27 -0400

    more in and out tests

--- public/js/script_001.js
+++ public/js/script_001.js
@@ -4,28 +4,43 @@ inButtonText = IN_BUTTON.querySelector("h3");
 IN_BUTTON.style.cursor = "pointer";
 OUT_BUTTON.style.cursor = "pointer";
 const TIMER = document.querySelector("#timer");
+const DIGITAL_HRS = document.querySelector("#digital_hrs");
+const DIGITAL_MIN = document.querySelector("#digital_min");
+const DIGITAL_SEC = document.querySelector("#digital_sec");
 
-var date = new Date();
-var hr = date.getHours();
-var min = date.getMinutes();
-var sec = date.getSeconds();
+// var new_date = new Date();
+var duration = 0;
+var frame = 1000;
+
 
 function setInPoint (e) {
   e.preventDefault();
   IN_BUTTON.classList.toggle("red");
   IN_BUTTON.classList.toggle("blue");
-  var date = Date.now();
-  runTimer(date);
+  var new_date = Date.now();
   console.log("in button pushed at " + Date.now());
   var xhr = new XMLHttpRequest();
   xhr.open("POST", '/in_out', true);
   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhr.send(JSON.stringify({ in_point: date, text: "test text" }));
+  xhr.send(JSON.stringify({ in_point: new_date, text: "test text" }));
 }
 
-function runTimer (date) {
 
-}
+
+function runTimer () {
+    var multiplier = 1000/1000;
+    var new_date = new Date();
+    let hr = new_date.getHours();
+    let min = new_date.getMinutes();
+    let sec = new_date.getSeconds();
+    var zerofilled_hr = ('00'+hr).slice(-2);
+    var zerofilled_min = ('00'+min).slice(-2);
+    var zerofilled_sec = ('00'+sec).slice(-2);
+    DIGITAL_HRS.innerHTML = zerofilled_hr + ":";
+    DIGITAL_MIN.innerHTML = zerofilled_min + ":";
+    DIGITAL_SEC.innerHTML = zerofilled_sec;
+
+  }
 
 function setOutPoint (e) {
   e.preventDefault();
@@ -41,11 +56,18 @@ function setOutPoint (e) {
   xhr.send(JSON.stringify({ out_point: Date.now(), text: "test text" }));
 }
 
+function interval() {
+  setInterval(runTimer, frame);
+}
+
 
 
 IN_BUTTON.addEventListener("click", setInPoint, false);
+IN_BUTTON.addEventListener("click", interval, false);
+
 // IN_BUTTON.addEventListener("click", function () {
 //   console.log("button pushed at " + Date.now());
 // }, false);
 
 OUT_BUTTON.addEventListener("click", setOutPoint, false);
+OUT_BUTTON.addEventListener("click", clearInterval(interval), false);
--- routes/in_out.js
+++ routes/in_out.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('../models/moment');
+
 // var bodyParser = require('body-parser');
 
 // router.use(bodyParser.json());
